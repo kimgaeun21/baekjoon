920424a8823137867738f7f5cd7863ff04ed17a8
GaEun <80972215+kimgaeun21@users.noreply.github.com>
2024-01-22 15:56:22 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+if n == 1:
+    print(1)
+elif n==2:
+    print(3)
+else:
+    arr = [0 for _ in range(n+1)]
+    arr[1] = 1
+    arr[2] = 3
+    for i in range(3, n+1):
+        arr[i] = (arr[i-1] + arr[i-2]*2)%10007
+    print(arr[n])
