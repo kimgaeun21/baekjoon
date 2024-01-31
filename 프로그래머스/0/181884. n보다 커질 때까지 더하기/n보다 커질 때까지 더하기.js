69d967b1b9f4834ad173ee7050cfc8270f44b2d8
GaEun <80972215+kimgaeun21@users.noreply.github.com>
2024-01-31 16:46:24 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers, n) {
+    var answer = 0;
+    for(a of numbers){
+        answer += a
+        if (answer > n){
+            return answer
+        }
+    }
+    return answer;
+}
