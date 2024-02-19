156aa1f8da3813a80b47aa46b38eb7f1d5fd0914
GaEun <80972215+kimgaeun21@users.noreply.github.com>
2024-02-20 00:16:14 +0900

    [Silver I] Title: 오르막 수, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -1,13 +1,12 @@
 n = int(input())
-dp = [[0 for _ in range(10)] for _ in range(n+1)]
+dp = [[0 for _ in range(10)] for _ in range(n)]
+dp[0] = [1,1,1,1,1,1,1,1,1,1]
 
-for i in range(10):
-    dp[1][i] = 1
-
-if n == 1:
-    print(10)
+if n==1:
+    print(sum(dp[0]))
 else:
-    for i in range(2, n+1):
+    for i in range(1,n):
         for j in range(10):
-            dp[i][j] = sum(dp[i-1][0:j+1])
-    print(sum(dp[n])%10007)
+            dp[i][j] = sum(dp[i-1][:j+1])%10007
+    
+    print(sum(dp[n-1])%10007)
