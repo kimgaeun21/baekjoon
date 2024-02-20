a3b999e400bb97bcb10bcd325e472d0f19cfccff
GaEun <80972215+kimgaeun21@users.noreply.github.com>
2024-02-21 01:18:40 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,17 +1,33 @@
-n = int(input())
-dp = [[0 for _ in range(10)] for _ in range(n + 1)]
-for i in range(1,10):
-    dp[1][i] = 1
+# n = int(input())
+# dp = [[0 for _ in range(10)] for _ in range(n + 1)]
+# for i in range(1,10):
+#     dp[1][i] = 1
 
-if n == 1: 
-    print(sum(dp[1]))
-else:
-    for i in range(2, n+1):
-        for j in range(10):
-            if j == 0:
-                dp[i][j] = dp[i-1][1]
-            elif j == 9:
-                dp[i][j] = dp[i-1][8]
-            else:
-                dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
-    print(sum(dp[n])%1000000000)
+# if n == 1: 
+#     print(sum(dp[1]))
+# else:
+#     for i in range(2, n+1):
+#         for j in range(10):
+#             if j == 0:
+#                 dp[i][j] = dp[i-1][1]
+#             elif j == 9:
+#                 dp[i][j] = dp[i-1][8]
+#             else:
+#                 dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+#     print(sum(dp[n])%1000000000)
+
+
+n = int(input())
+dp= [[0 for i in range(10)] for i in range(n)]
+dp[0] = [0,1,1,1,1,1,1,1,1,1]
+
+for i in range(1,n):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][j+1] %1000000000
+        elif j==9:
+            dp[i][j] = dp[i-1][j-1] %1000000000
+        else:
+            dp[i][j] = (dp[i-1][j+1] + dp[i-1][j-1])%1000000000
+            
+print(sum(dp[n-1])%1000000000)
