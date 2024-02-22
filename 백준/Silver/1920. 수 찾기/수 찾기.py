47cd596af88cc9200b7dc016dfcaf4f3d6cc3917
GaEun <80972215+kimgaeun21@users.noreply.github.com>
2024-02-23 01:57:10 +0900

    [Silver IV] Title: 수 찾기, Time: 168 ms, Memory: 50380 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,26 +1,11 @@
-
-
 n = int(input())
-n_arr = list(map(int, input().split()))
-n_arr.sort()
+n_arr = set(map(int, input().split()))
 
 m = int(input())
 m_arr = list(map(int, input().split()))
 
-def find(a):
-    start = 0 
-    end =n -1
-    
-    while start <= end:
-        mid = (start+end)//2
-        if  n_arr[mid] == a:
-            return 1
-        elif n_arr[mid] < a:
-            start = mid+1
-        else:
-            end = mid-1
-    return 0        
-        
 for i in m_arr:
-    print(find(i))
-    
+    if i in n_arr:
+        print(1)
+    else:
+        print(0)
