602721a508e4d1ab00c80a2fb84e32e4c15adf7b
GaEun <80972215+kimgaeun21@users.noreply.github.com>
2024-02-22 12:44:04 +0900

    [level 0] Title: 평행, Time: 0.09 ms, Memory: 33.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.js
@@ -0,0 +1,22 @@
+function solution(dots) {
+  var answer = 0;
+
+  function cal(a, b, c, d) {
+    let x, y;
+
+    x = (b[1] - a[1]) / (b[0] - a[0]);
+    y = (d[1] - c[1]) / (d[0] - c[0]);
+
+    if (x === y) {
+      answer += 1;
+    }
+  }
+
+  cal(dots[0], dots[1], dots[2], dots[3]);
+
+  cal(dots[0], dots[2], dots[1], dots[3]);
+
+  cal(dots[0], dots[3], dots[1], dots[2]);
+
+  return answer > 0 ? 1 : 0;
+}
